441cd4fdd58de55075d743ca69f10cfb4878882d
Leonardo_Lo_medico <leonardo.lomedico@hotmail.it>
2022-11-22 12:36:33 +0100

    spliceComponenti

--- src/Lista.js
+++ src/Lista.js
@@ -1,5 +1,8 @@
 import listaOggetti from "./ListaOggetti";
 function Lista(){
+    const array = [1,2,3,4,5,6,7,8,9,10];
+    const array1 =array.map((numero)=> numero * 2);
+    console.log(array1);
     console.log(listaOggetti);
     return (
         <ul>
